cda25eb4b6f30225e0e6a41f490aa28d058c0645
David <ddkpham@gmail.com>
2019-03-23 14:26:41 -0700

    add react-router and skeleton for links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {BrowserRouter} from 'react-router-dom';
+
 import classes from './App.module.css';
 import Layout from './component/Layout/Layout'
 
@@ -8,14 +10,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className={classes.App}>
-        <Layout>
+      <BrowserRouter>
+        <div className={classes.App}>
+          <Layout>
           
-        </Layout>
-        <footer className={classes.Footer}>
-          <p>Copyright &copy;2018 Design by David Pham | Subin Bae</p>
-        </footer>
-      </div>
+          </Layout>
+          <footer className={classes.Footer}>
+            <p>Copyright &copy;2018 Design by David Pham | Subin Bae | Paul Miller</p>
+          </footer>
+        </div>
+      </BrowserRouter>
+      
     );
   }
 }
