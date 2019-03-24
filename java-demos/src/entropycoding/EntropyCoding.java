4483c091c56e0c50e065df225b320849d3ff6058
Paul <pmiiller@sfu.ca>
2019-03-24 15:18:12 -0700

    Added encoding and decoding for the huffman coding

--- java-demos/src/entropycoding/EntropyCoding.java
+++ java-demos/src/entropycoding/EntropyCoding.java
@@ -8,17 +8,21 @@ public class EntropyCoding {
     public static void main(String[] args) {
         ArithCoding arithmeticCoding = new ArithCoding();
         arithmeticCoding.initDefaultSymbols();
-        
-        String inputString = "CAEE$";
-        long encoding = arithmeticCoding.encode(inputString);
-        System.out.println("Value: " + arithmeticCoding.getCodewordValue(encoding));
-        System.out.println(String.format("Encoding: %x", encoding));
-        String output = arithmeticCoding.decode(encoding);
-        System.out.println("Decoding: " + output);
-        
+
+        // String inputString = "CAEE$";
+        // long encoding = arithmeticCoding.encode(inputString);
+        // System.out.println("Value: " + arithmeticCoding.getCodewordValue(encoding));
+        // System.out.println(String.format("Encoding: %x", encoding));
+        // String output = arithmeticCoding.decode(encoding);
+        // System.out.println("Decoding: " + output);
+
         HuffCoding huffmanCoding = new HuffCoding();
         huffmanCoding.createHuffmanTree("HELLO");
-        
+        String encoding = huffmanCoding.encode("HELLO");
+        System.out.println(encoding);
+        String decoding = huffmanCoding.decode(encoding);
+        System.out.println(decoding);
+
     }
-    
+
 }
--- java-demos/src/entropycoding/huffman/HuffCoding.java
+++ java-demos/src/entropycoding/huffman/HuffCoding.java
@@ -7,33 +7,37 @@ import java.util.Map;
 import java.util.List;
 
 public class HuffCoding {
-    
+
     PriorityQueue<HuffTree> subtreeQueue;
-    HashMap<Character, Integer> characterFrequency;
+    HashMap<Character, Integer> symbolFrequency;
     List<HuffTree> symbolList;
-    
+    HashMap<Character, String> symbolToCodeword;
+    HashMap<String, Character> codewordToSymbol;
+
     public HuffCoding () {
-        subtreeQueue = new PriorityQueue<>(5, new HuffComparator()); 
-        characterFrequency = new HashMap<>();
-        symbolList = new ArrayList<HuffTree>(); 
+        subtreeQueue = new PriorityQueue<>(5, new HuffComparator());
+        symbolFrequency = new HashMap<>();
+        symbolList = new ArrayList<HuffTree>();
+        symbolToCodeword = new HashMap<>();
+        codewordToSymbol = new HashMap<>();
     }
-    
+
     public void createHuffmanTree (String input) {
-        
+
         // Get the frequency for each character
         for (int i = 0; i < input.length(); i++) {
             char currChar = input.charAt(i);
-            characterFrequency.putIfAbsent(currChar, 0);
-            characterFrequency.put(currChar, characterFrequency.get(currChar) + 1);
+            symbolFrequency.putIfAbsent(currChar, 0);
+            symbolFrequency.put(currChar, symbolFrequency.get(currChar) + 1);
         }
-        
+
         // Put each character into priority queue based on frequency
-        for (Map.Entry<Character, Integer> entry : characterFrequency.entrySet()) {
+        for (Map.Entry<Character, Integer> entry : symbolFrequency.entrySet()) {
             HuffTree newTree = new HuffTree(entry.getKey(), entry.getValue());
             subtreeQueue.add(newTree);
             symbolList.add(newTree);
         }
-        
+
         // Create the huffman tree
         while (subtreeQueue.size() > 1) {
             HuffTree subtree1 = subtreeQueue.poll();
@@ -45,25 +49,48 @@ public class HuffCoding {
             subtree2.setParent(newSubtree);
             subtreeQueue.add(newSubtree);
         }
-        
+
         // Get the huffman codeword for each symbol
         for (int i = 0; i < symbolList.size(); i++) {
             HuffTree node = symbolList.get(i);
             HuffTree currNode = node;
             HuffTree prevNode = null;
-            String codeWord = "";
+            String codeword = "";
             while (currNode.getParent() != null) {
                 prevNode = currNode;
                 currNode = currNode.getParent();
                 if (prevNode == currNode.getLeft()) {
-                    codeWord = "0" + codeWord;
+                    codeword = "0" + codeword;
                 } else if (prevNode == currNode.getRight()) {
-                    codeWord = "1" + codeWord;
+                    codeword = "1" + codeword;
                 } else {
                     System.out.println("BAD");
                 }
             }
-            System.out.println(node.symbol + ": " + codeWord);
+            symbolToCodeword.put(node.getSymbol(), codeword);
+            codewordToSymbol.put(codeword, node.getSymbol());
         }
+    }
+
+    public String encode (String input) {
+        String output = "";
+        for (int i = 0; i < input.length(); i++) {
+            char currChar = input.charAt(i);
+            output = output + symbolToCodeword.get(currChar);
+        }
+        return output;
+    }
+
+    public String decode (String input) {
+        String output = "";
+        String currCodeword = "";
+        for (int i = 0; i < input.length(); i++) {
+            currCodeword = currCodeword + input.charAt(i);
+            if (codewordToSymbol.containsKey(currCodeword)) {
+                output = output + codewordToSymbol.get(currCodeword);
+                currCodeword = "";
+            }
+        }
+        return output;
     }
 }
