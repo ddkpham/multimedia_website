cc4d1023ba60a2ae0969841ce7a158819d93b0bc
ddkpham <dpa35@sfu.ca>
2019-03-25 21:34:42 -0700

    Updates

---
+++ precache-manifest.758c00fe8605d41c1382158af9c3eaf0.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f0275bc322980be6e48",
+    "url": "/multimedia_website/static/css/main.f5cca809.chunk.css"
+  },
+  {
+    "revision": "1f0275bc322980be6e48",
+    "url": "/multimedia_website/static/js/main.a3146f91.chunk.js"
+  },
+  {
+    "revision": "4e7ba8cd0cc339117b15",
+    "url": "/multimedia_website/static/js/runtime~main.259f39c0.js"
+  },
+  {
+    "revision": "1c229f20607b98e3cb96",
+    "url": "/multimedia_website/static/js/2.ccb1da33.chunk.js"
+  },
+  {
+    "revision": "7d91755166fd557af35e3dc8a36dad08",
+    "url": "/multimedia_website/static/media/dark-paths.7d917551.png"
+  },
+  {
+    "revision": "fdff6feca26e6f410e48b5771e41f710",
+    "url": "/multimedia_website/static/media/data.fdff6fec.png"
+  },
+  {
+    "revision": "50a42a7f3a523f016708f8c6fe5453f5",
+    "url": "/multimedia_website/static/media/Chapter1.50a42a7f.pdf"
+  },
+  {
+    "revision": "aa96f48cf4c67eeb8dc91642f0622a07",
+    "url": "/multimedia_website/static/media/sfulogo.aa96f48c.jpg"
+  },
+  {
+    "revision": "b1609666347351ad2b3941f9c3782999",
+    "url": "/multimedia_website/static/media/Intro.b1609666.pdf"
+  },
+  {
+    "revision": "78b2ac5063951671944f06d054972ecd",
+    "url": "/multimedia_website/static/media/Chapter2.78b2ac50.pdf"
+  },
+  {
+    "revision": "9a70c6a776e7941e9c9194347fa1bf69",
+    "url": "/multimedia_website/static/media/Chapter3.9a70c6a7.pdf"
+  },
+  {
+    "revision": "583b7f38b11e35bf08e94c287a443f0b",
+    "url": "/multimedia_website/static/media/Chapter4.583b7f38.pdf"
+  },
+  {
+    "revision": "c11a8a748bef7358ccaa16231f3b92e8",
+    "url": "/multimedia_website/static/media/Chapter5.c11a8a74.pdf"
+  },
+  {
+    "revision": "9c2eb65659205aaec8b0c4743f578405",
+    "url": "/multimedia_website/static/media/Chapter6.9c2eb656.pdf"
+  },
+  {
+    "revision": "902d7708775f41ff7f19a642964d394d",
+    "url": "/multimedia_website/static/media/Chapter7.902d7708.pdf"
+  },
+  {
+    "revision": "d570e04e15a9ca53b6bb5c9f6495a1e5",
+    "url": "/multimedia_website/static/media/Chapter8.d570e04e.pdf"
+  },
+  {
+    "revision": "df16a697e16b0a09756fabb59fb04a95",
+    "url": "/multimedia_website/static/media/Chapter9.df16a697.pdf"
+  },
+  {
+    "revision": "dd489165418016fe3ab592449027ec94",
+    "url": "/multimedia_website/static/media/Chapter10.dd489165.pdf"
+  },
+  {
+    "revision": "c75e702ce8baae4c50b276ed4b40c715",
+    "url": "/multimedia_website/static/media/Chapter11.c75e702c.pdf"
+  },
+  {
+    "revision": "e7057d74bfdfadf7d9b1bce35c63a515",
+    "url": "/multimedia_website/static/media/Chapter12.e7057d74.pdf"
+  },
+  {
+    "revision": "648d0a1ac4a4d9184eb8375dc1fc78a6",
+    "url": "/multimedia_website/static/media/Chapter14.648d0a1a.pdf"
+  },
+  {
+    "revision": "601df5ff745cb1355df1f41e5dfab002",
+    "url": "/multimedia_website/static/media/Week1.601df5ff.pdf"
+  },
+  {
+    "revision": "8e938540bda23977a80291590ccd664f",
+    "url": "/multimedia_website/static/media/Week2.8e938540.pdf"
+  },
+  {
+    "revision": "e296a0e7194a6b13755f3342e7fb22fc",
+    "url": "/multimedia_website/static/media/Week3.e296a0e7.pdf"
+  },
+  {
+    "revision": "c577a824447e69d61c02ac92fc983a9c",
+    "url": "/multimedia_website/static/media/Week4.c577a824.pdf"
+  },
+  {
+    "revision": "9ccdc47abf6d897d67bf4889d3e408e6",
+    "url": "/multimedia_website/static/media/Week5.9ccdc47a.pdf"
+  },
+  {
+    "revision": "c2b5481a72f1f41bc4f022bb1fdf13fc",
+    "url": "/multimedia_website/static/media/Week6.c2b5481a.pdf"
+  },
+  {
+    "revision": "27c6dda682d27d2fe8785b3bbf43b567",
+    "url": "/multimedia_website/static/media/Week7.27c6dda6.pdf"
+  },
+  {
+    "revision": "5ede2dd3afa1cb447001722f41da20f3",
+    "url": "/multimedia_website/static/media/Week9.5ede2dd3.pdf"
+  },
+  {
+    "revision": "ad0a6e665bc04885a150eae480dea7a4",
+    "url": "/multimedia_website/static/media/Week10.ad0a6e66.pdf"
+  },
+  {
+    "revision": "ce25c16df5e11c874c0ef2fecf3fbccd",
+    "url": "/multimedia_website/static/media/Week10_2.ce25c16d.pdf"
+  },
+  {
+    "revision": "5c9a33a61c7819dd7e329ae8e9941182",
+    "url": "/multimedia_website/static/media/Week11.5c9a33a6.pdf"
+  },
+  {
+    "revision": "f772716ea6e694a81a4139f67f6124c2",
+    "url": "/multimedia_website/static/media/Week12.f772716e.pdf"
+  },
+  {
+    "revision": "7d4d4e0de3d9c3521637205b8f2ccd82",
+    "url": "/multimedia_website/static/media/Week12_2.7d4d4e0d.pdf"
+  },
+  {
+    "revision": "1c0121907f602f2d5101b6a59c4973d8",
+    "url": "/multimedia_website/static/media/365-term-project-topics.1c012190.pdf"
+  },
+  {
+    "revision": "9eec37f0efbc0f8044645476e284e741",
+    "url": "/multimedia_website/static/media/library.9eec37f0.png"
+  },
+  {
+    "revision": "b574f297ed9a4b4c12f693057b3ebe49",
+    "url": "/multimedia_website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/multimedia_website/precache-manifest.d3b8dfd300cd86f509caa2aa2baf9c89.js"
+  "/multimedia_website/precache-manifest.758c00fe8605d41c1382158af9c3eaf0.js"
 );
 
 workbox.clientsClaim();
